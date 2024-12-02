3724db4dabb70bb2450d09d567d6515a3799b2d9
Noah Mecham <nmecham27@gmail.com>
2024-12-02 12:52:32 -0700

    Finished last task.

--- server/gatt_counter.c
+++ server/gatt_counter.c
@@ -55,6 +55,7 @@
 #include "gatt_counter.h"
 #include "btstack.h"
 #include "ble/gatt-service/battery_service_server.h"
+#include "temp_sense.h"
 
 #define HEARTBEAT_PERIOD_MS 1000
 
@@ -220,6 +221,14 @@ static uint16_t att_read_callback(hci_con_handle_t connection_handle, uint16_t a
 
     if (att_handle == ATT_CHARACTERISTIC_0000FF11_0000_1000_8000_00805F9B34FB_01_VALUE_HANDLE){
         return att_read_callback_handle_blob((const uint8_t *)counter_string, counter_string_len, offset, buffer, buffer_size);
+    }
+    else if( att_handle == ATT_CHARACTERISTIC_ORG_BLUETOOTH_SERVICE_DEVICE_INFORMATION_01_VALUE_HANDLE )
+    {
+        printf("Reading temperature\n");
+        uint16_t value = (uint16_t)(temperature_poll());
+        printf("temperature %d\n", value);
+        value = (value >> 8) | (value << 8); 
+        return att_read_callback_handle_blob((uint8_t*)(&value), sizeof(uint16_t), offset, buffer, buffer_size);
     }
     return 0;
 }
--- server/picow_bt_example_freertos.c
+++ server/picow_bt_example_freertos.c
@@ -12,6 +12,7 @@
 #include "btstack_event.h"
 #include "pico/cyw43_arch.h"
 #include "picow_bt_example_common.h"
+#include "temp_sense.h"
 
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
@@ -33,6 +34,8 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
             if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
             gap_local_bd_addr(local_addr);
             printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
+            temperature_setup();
+            printf("Temperature setup complete\n");
             break;
         case BTSTACK_EVENT_NR_CONNECTIONS_CHANGED:
             // if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) return;
